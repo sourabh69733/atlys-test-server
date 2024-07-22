27e97442a1768a5b33547225c3730be9ea34d3b4
Sourabh <sourabhsahu69733@gmail.com>
2024-07-22 15:44:10 +0530

    feat: post request for scraping product data

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from database import create_database
+from scrape_catalogue import scrape_catalogue
 
 app = FastAPI()
 
+DATABASE_NAME = 'products.production.db'
+
+def init_db_services() -> bool:
+    create_database(DATABASE_NAME)
+    return True
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/scrape")
+def scrape_catalogue_endpoint(url: str, num_pages: int, proxy: Union[str, None] = None):
+    try:
+        init_db_services()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+        products, inserted_count, existing_count = scrape_catalogue(DATABASE_NAME, url, num_pages, proxy_string=proxy)
+        return {
+            "status": "ok",
+            "inserted_count": inserted_count,
+            "existing_count": existing_count,
+            "total_products": len(products)
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to scrape catalogue: {e}")
